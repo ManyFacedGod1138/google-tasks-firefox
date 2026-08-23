b907ef6a03be5c04a924f18e5955377d2746562a
ManyFacedGod1138 <58525725+ManyFacedGod1138@users.noreply.github.com>
2026-08-23 18:56:23 +0300

    chore: configure TypeScript build

---
+++ src/popup/popup.ts
@@ -0,0 +1,11 @@
+const openDashboardButton = document.getElementById("open-dashboard");
+
+openDashboardButton?.addEventListener("click", () => {
+    const dashboardUrl = browser.runtime.getURL(
+        "dashboard/dashboard.html"
+    );
+
+    browser.tabs.create({
+        url: dashboardUrl
+    });
+});
